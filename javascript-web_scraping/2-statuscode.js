ae4f23f3e32ebb732588e89878e9da36fb2f594a
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-05-14 15:10:21 +0400

    Statement check

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -2,6 +2,10 @@
 
 const request = require('request');
 
-request(process.argv[2], (err, response) => {
-  console.log('code: ', response.statusCode);
+request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('code:', response && response.statusCode);
+  }
 });

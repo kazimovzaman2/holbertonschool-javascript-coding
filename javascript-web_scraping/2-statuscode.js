ff85abaab569df91a0a348dfd56d40693174e89c
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-05-14 15:07:55 +0400

    Space after 'code'|

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -3,5 +3,5 @@
 const request = require('request');
 
 request(process.argv[2], (err, response) => {
-  console.log('code:', response.statusCode);
+  console.log('code: ', response.statusCode);
 });

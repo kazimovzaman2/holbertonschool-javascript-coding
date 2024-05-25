a575d867a0f0cf7f5a55fed6e9d0f78588c4bb2f
kazimovzaman2 <kazimovzaman2@gmail.com>
2024-05-25 13:17:02 +0400

    tasks

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,22 +1,15 @@
-// Display the initial message
-console.log("Welcome to Holberton School, what is your name?");
-
-// Set encoding to utf-8 for proper string handling
-process.stdin.setEncoding('utf-8');
-
-// Listen for user input
-process.stdin.on('data', (data) => {
-  // Remove any trailing newline characters from the input
-  const name = data.trim();
-
-  // Display the inputted name
-  console.log(`Your name is: ${name}`);
-
-  // End the process
-  process.exit();
-});
-
-// Listen for process exit event
-process.on('exit', () => {
-  console.log('This important software is now closing');
+console.log('Welcome to Holberton School, what is your name?');
+// await input
+process.stdin.resume();
+// once input is entered
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  process.stdout.write(`Your name is: ${name}`);
+  // check if input is coming from the terminal
+  if (process.stdin.isTTY) {
+    process.exit();
+  } else {
+    process.stdout.write('This important software is now closing\n');
+    process.exit();
+  }
 });
